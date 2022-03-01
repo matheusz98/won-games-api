183ee6487025ee5373405a394e5f85c1f17d8f4a
Matheus Costa <matheuscastiglia31@gmail.com>
2022-03-01 11:37:53 -0300

    Criando a primeira estrutura de jogos

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -14,11 +14,20 @@ async function getGameInfo(slug) {
   const body = await axios.get(`https://www.gog.com/game/${slug}`);
   const dom = new JSDOM(body.data);
 
+  const ratingElement = dom.window.document.querySelector(
+    ".age-restrictions__icon use"
+  );
+
   const description = dom.window.document.querySelector(".description");
 
   return {
-    rating: "BR0",
-    short_description: description.textContent.slice(0, 160),
+    rating: ratingElement
+      ? ratingElement
+          .getAttribute("xlink:href")
+          .replace(/_/g, "")
+          .replace(/[^\w-]+/g, "")
+      : "BR0",
+    short_description: description.textContent.trim().slice(0, 160),
     description: description.innerHTML,
   };
 }
@@ -68,6 +77,40 @@ async function createManyToManyData(products) {
   ]);
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -75,13 +118,7 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    // console.log(products[0]);
-
-    await createManyToManyData([products[0], products[1]]);
-
-    // await create(products[0].publisher, "publisher");
-    // await create(products[0].developer, "developer");
-
-    // console.log(await getGameInfo(products[0].slug));
+    await createManyToManyData([products[2], products[3]]);
+    await createGames([products[2], products[3]]);
   },
 };
