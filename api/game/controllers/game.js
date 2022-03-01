eeef61a261054e49a96d55bc5889dbe8384c86ba
Matheus Costa <matheuscastiglia31@gmail.com>
2022-03-01 16:10:14 -0300

    Finalizando a API

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
@@ -9,7 +9,13 @@ module.exports = {
   populate: async (ctx) => {
     console.log("Starting to populate...");
 
-    await strapi.services.game.populate();
+    const options = {
+      sort: "popularity",
+      page: "1",
+      ...ctx.query,
+    };
+
+    await strapi.services.game.populate(options);
 
     ctx.send("Finished populating!");
   },
